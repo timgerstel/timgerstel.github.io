b3e12def36589c7adc21fa856854160edfa7fda9
Timothy Gerstel <tim.gerstel@gmail.com>
2025-03-21 00:10:52 -0400

    Make menu bar transparent
    Signed-off-by: Timothy Gerstel <tim.gerstel@gmail.com>

--- tg-blog/src/app/menubar.tsx
+++ tg-blog/src/app/menubar.tsx
@@ -6,12 +6,12 @@ import Link from "next/link";
 export default function MenuBar(){
   
 
-  return (<div className="w-full h-[50px] border-b border-dashed border-[rgba(0,132,255,0.1)] sticky top-0 z-50 bg-slate-100 dark:bg-[#17161b]">
+  return (<div className="w-full h-[50px] border-b border-dashed border-[rgba(0,132,255,0.1)] sticky top-0 z-50 bg-[#f1f5f973] dark:bg-[#17161b5e] backdrop-blur-sm">
     <div className="flex items-center gap-2 md:gap-3
       whitespace-nowrap
       h-full
-      ml-5 md:ml-10
-      mr-5 md:mr-10
+      ml-5 md:ml-16
+      mr-5 md:mr-16
       pl-3 pr-3
       border-l border-r border-dashed border-[rgba(0,132,255,0.1)]
     font-bold">
